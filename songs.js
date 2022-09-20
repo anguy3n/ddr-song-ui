a2f9921e34656a6845f1e4b87e50ad3e9a601611
Alyssa Nguyen <alyssa_nguyen@berkeley.edu>
2022-09-20 01:26:39 -0700

    formatted

--- songs.js
+++ songs.js
@@ -6,20 +6,20 @@ async function fetch_json() {
     return songs;
 }
 
-// function sort_by_pack(songs) {
-//     var packs_sorted = {};
-//     for (var song in songs) {
-//         let song_info = songs[song];
-//         let pack = song_info["pack"];
-//         if (pack in packs_sorted) {
-//             packs_sorted[pack][song] = song_info
-//         } else {
-//             packs_sorted[pack] = {};
-//             packs_sorted[pack][song] = song_info
-//         }
-//     }
-//     return packs_sorted;
-// }
+function sort_by_pack(songs) {
+    var packs_sorted = {};
+    for (var song in songs) {
+        let song_info = songs[song];
+        let pack = song_info["pack"];
+        if (pack in packs_sorted) {
+            packs_sorted[pack][song] = song_info
+        } else {
+            packs_sorted[pack] = {};
+            packs_sorted[pack][song] = song_info
+        }
+    }
+    return packs_sorted;
+}
 
 // function sort_alphabetical_title(songs) {
 //     var alphabetically_sorted = {};
@@ -56,24 +56,11 @@ async function fetch_json() {
 //     return alphabetically_sorted;
 // }
 
-function insert_content(pack, content) {
-    for (song in pack) {
-        content.innerHTML += song + "<br>"
-        content.innerHTML += "<ul>" +
-            "<li>" + artist + "</li>" +
-            "<li>" + pack_name + "</li>" +
-            "<li>" + difficulties + "</li>" +
-            "</ul>"
-
-    }
-}
-
-function load_components(songs) {
-    console.log(songs)
-    for (song in songs) {
-        var artist = songs[song]["artist"];
-        var folder_name = songs[song]["pack"]
-        var difficulties = songs[song]["difficulties"].sort((a, b) => a - b);
+function insert_content(folder_songs, song_list) {
+    for (song in folder_songs) {
+        var artist = folder_songs[song]["artist"];
+        var folder_name = folder_songs[song]["pack"]
+        var difficulties = folder_songs[song]["difficulties"].sort((a, b) => a - b);
         var song_container = document.createElement("div");
         song_container.className = "song-container";
         var song_content = document.createElement("div");
@@ -81,9 +68,18 @@ function load_components(songs) {
         var song_title = document.createElement("h2");
         song_title.className = "song-title";
         song_title.innerHTML = song;
+        if (song.length > 15) {
+            song_title.innerHTML = song.slice(0, 15) + '...';
+        } else {
+            song_title.innerHTML = song;
+        }
         var song_artist = document.createElement("h5");
         song_artist.className = "song-artist";
-        song_artist.innerHTML = artist
+        if (artist.length > 15) {
+            song_artist.innerHTML = artist.slice(0, 15) + '...';
+        } else {
+            song_artist.innerHTML = artist;
+        }
         var language = document.createElement("p");
         language.innerHTML = "English";
         var folder = document.createElement("p");
@@ -104,53 +100,46 @@ function load_components(songs) {
         song_content.appendChild(song_artist);
         song_content.appendChild(characteristics);
         song_content.appendChild(difficulties_components);
-        song_container.appendChild(song_content)
-        document.getElementById("body").appendChild(song_container);
+        song_container.appendChild(song_content);
+        song_list.appendChild(song_container);
     }
 }
 
-async function refresh() {
+function create_collapsibles(sorted_songs) {
+    for (category in sorted_songs) {
+        var button = document.createElement("button");
+        button.className = "collapsible"
+        button.innerText = category;
+        var song_list = document.createElement("div");
+        song_list.className = "song-list";
+        insert_content(sorted_songs[category], song_list);
+        var pack_and_songs = document.createElement("div");
+        pack_and_songs.className = "pack-and-songs";
+        pack_and_songs.appendChild(button);
+        pack_and_songs.appendChild(song_list);
+        document.getElementById("body").appendChild(pack_and_songs)
+
+    }
+}
+async function filter(filter_func) {
     var songs = await fetch_json();
-    load_components(songs);
+    document.getElementById("body").innerHTML = "";
+    await create_collapsibles(filter_func(songs))
 }
 
-refresh();
-
-// function create_collapsibles(sorted_songs) {
-//     for (category in sorted_songs) {
-//         var button = document.createElement("button");
-//         button.className = "collapsible"
-//         button.innerText = category;
-//         var div_content = document.createElement("div");
-//         div_content.className = "content";
-//         var content = document.createElement("p");
-//         insert_content(sorted_songs[category], content);
-//         document.getElementById("body").appendChild(button);
-//         div_content.appendChild(content);
-//         document.getElementById("body").appendChild(div_content);
-//     }
-// }
-// async function filter(filter_func) {
-//     var songs = await fetch_json();
-//     document.getElementById("body").innerHTML = "";
-//     await create_collapsibles(filter_func(songs))
-// }
-
-// filter(sort_by_pack);
-// setTimeout(() => {
-//     var coll = document.getElementsByClassName("collapsible");
-//     console.log(coll)
-//     var i;
-
-//     for (i = 0; i < coll.length; i++) {
-//         coll[i].addEventListener("click", function() {
-//             this.classList.toggle("active");
-//             var content = this.nextElementSibling;
-//             if (content.style.display === "block") {
-//                 content.style.display = "none";
-//             } else {
-//                 content.style.display = "block";
-//             }
-//         });
-//     }
-// }, 200);
+filter(sort_by_pack);
+setTimeout(() => {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}, 200);
